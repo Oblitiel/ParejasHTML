901185c4deab3eecdbe553957e2848ed60f55372
Oblitiel <gabrinator99@gmail.com>
2025-02-13 12:49:35 +0100

    refactorizacion

--- juego/scripts/game.js
+++ juego/scripts/game.js
@@ -1,27 +1,19 @@
-var cards = []
 var row
 var col
 var tapped = []
 var checking = false
+var solvedPairs = 0
+
 
 function runGame(){
     row = document.getElementById("in_fila").value
     col = document.getElementById("in_col").value
     prepareGame(col, row)
-    poblateCards(cards)
+    // poblateCards(cards)
 }
 
-function prepareGame(){
-    if (((row * col) % 2 ) != 0) {
-        var parrafo = document.createElement("p")
-        var text = document.createTextNode("No son unos valores de pareja validos")
-        parrafo.appendChild(text)
-        // var div = document.getElementById("input_div")
-        // div.appendChild(parrafo)
-        alert("No son unos valores de pareja validos")
-        return
-    }
-    if (row<0 || col<0){
+function prepareGame(col, row){
+    if (((row * col) % 2 ) != 0 || row<0 || col<0) {
         alert("No son unos valores de pareja validos")
         return
     }
@@ -29,33 +21,32 @@ function prepareGame(){
     // Esconde el div
     var div = document.getElementById("input_div")
     div.classList.add("invisible")
-    // Borrar el elemento
-    // document.getElementById("input_div").remove()
 
-    document.getElementById("table_div").appendChild(generateTable(col,row))
+    // Genera la tabla
+    let table = generateTable(col,row)
+    document.getElementById("table_div").appendChild(table)
 }
 
 function generateTable(col, row) {
     var table = document.createElement("table");
+    // Crear lista de los numeros aleatorios a poner
+    var nums = []
+    for (let index = 0; index < row*col/2; index++) {
+        nums.push(index)
+        nums.push(index)
+    }
 
     for (let i = 0; i < row; i++) {
         var tr = document.createElement("tr")
         
         for (let j = 0; j < col; j++) {
             let td = document.createElement("td")
-            let card = document.createElement("div")
-            card.classList.add("card")
-            card.classList.add("card-hide")
             
-            card.style.width = 50 / col + "vh"
-            card.style.height = 50 / col*1.25 + "vh"
 
-            card.setAttribute("onclick","flipAndCheck(this)")
-            cards.push(card)
+            let randIndex = Math.floor(Math.random()*nums.length)
+            let card = generateCard(nums[randIndex])
+            nums.splice(randIndex,1)
 
-            var p = document.createElement("p")
-            card.appendChild(p)
-            
             td.appendChild(card)
             tr.appendChild(td)
         }
@@ -65,19 +56,36 @@ function generateTable(col, row) {
     return table
 }
 
-function hideCards() {
-    cards.forEach(card => {
-        card.classList.remove("card-show")
-        card.classList.add("card-hide")
-    });
+function generateCard(num){
+    let card = document.createElement("div")
+    card.classList.add("card")
+    card.classList.add("card-hide")
+    
+    card.style.width = 50 / col + "vh"
+    card.style.height = 50 / col * 1.25 + "vh"
+
+    card.setAttribute("onclick","flipAndCheck(this)")
+
+    var p = document.createElement("p")
+    card.number = num
+    p.innerHTML = card.number
+    card.appendChild(p)
+    return card
 }
 
-function showCards() {
-    cards.forEach(card => {
-        card.classList.remove("card-hide")
-        card.classList.add("card-show")
-    });
-}
+// function hideCards() {
+//     cards.forEach(card => {
+//         card.classList.remove("card-show")
+//         card.classList.add("card-hide")
+//     });
+// }
+
+// function showCards() {
+//     cards.forEach(card => {
+//         card.classList.remove("card-hide")
+//         card.classList.add("card-show")
+//     });
+// }
 
 function flipAndCheck(card){
     if (!card.classList.contains("card-show") && !checking) {
@@ -114,22 +122,12 @@ function unFlip(pair){
 
 function isPair(pair){
     if (pair[0].number == pair[1].number) {
+        solvedPairs ++
+        // Comprobar que esta resuleto
+        if (solvedPairs == (col * row)/2) {
+            alert("Ganaste pive")
+        }
         return true
     }
     return false
-}
-
-function poblateCards(cards){
-    var nums = []
-    for (let index = 0; index < row*col/2; index++) {
-        nums.push(index)
-        nums.push(index)
-    }
-
-    cards.forEach(card => {
-        let randIndex = Math.floor(Math.random()*nums.length)
-        card.firstElementChild.innerHTML = nums[randIndex]
-        card.number = nums[randIndex]
-        nums.splice(randIndex,1)
-    });
 }
